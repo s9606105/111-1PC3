56d945f2356437d5fafebf7999444232f047765f
s9606105 <s9606105@gmail.com>
2022-10-02 17:54:02 +0800

    1111002 Write Answer.pdf and rar

---
+++ 111-1PC3/Test.aspx.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace _111_1PC3
+{
+    public partial class Test : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            int i_Clothes = 7;
+            int i_Hat = 8;
+            int i_Pants = 9;
+            int total = i_Clothes * 300 + i_Hat * 350 + i_Pants * 400;
+            Response.Write("總價: " + total + " 元");
+        }
+    }
+}
